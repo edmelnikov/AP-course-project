f98dea8e048076f65c05e4744629de901fcc9945
Evgeny <Kyberbote@gmail.com>
2021-03-24 21:13:25 +0300

    added a page for a single product

--- siteroot/shop/urls.py
+++ siteroot/shop/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('catalogue/', views.catalogue, name='catalogue')
+    path('catalogue/', views.catalogue, name='catalogue'),
+    path('<int:product_id>', views.get_product, name='product_by_id')  # how does it work
 ]
--- siteroot/shop/views.py
+++ siteroot/shop/views.py
@@ -9,5 +9,9 @@ def index(request):
 
 def catalogue(request):
     products = Product.objects.all()
-
     return render(request, 'shop/catalogue.html', {'products': products})
+
+
+def get_product(request, product_id):
+    product = Product.objects.get(id=product_id)
+    return render(request, 'shop/single_product.html', {'product': product})
