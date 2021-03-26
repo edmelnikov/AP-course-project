c6a160d8037224ce1b73c186c2cd1f235ef7f91c
Evgeny <Kyberbote@gmail.com>
2021-03-26 23:15:42 +0300

    added login support

--- siteroot/shop/urls.py
+++ siteroot/shop/urls.py
@@ -6,5 +6,8 @@ urlpatterns = [
     path('catalogue/', views.catalogue, name='catalogue'),
     path('<int:product_id>', views.get_product, name='product_by_id'),  # how does it work
     path('<int:product_id>/post_review', views.post_review, name='post_review'),
-	path('<int:product_id>/post_review/error', views.print_review_error, name='print_error')
+	path('<int:product_id>/post_review/error', views.print_review_error, name='print_error'),
+    path('register/', views.register_page, name='register'),
+    path('login/', views.log_in_page, name='login')
+
 ]
--- siteroot/shop/views.py
+++ siteroot/shop/views.py
@@ -1,13 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpResponseRedirect, Http404
 from .models import *
 from django.urls import reverse
+from  django.contrib.auth import login, logout, authenticate
+from .forms import LoginForm
 
 # TODO: добавить ошибку 404, добавить ошибку при попытке запостить пустой отзыв
 
 
 def index(request):
-    return render(request, 'shop/index.html')
+	if request.user.is_authenticated:  # проверка на аутентификацию пользователя
+		name = request.user.username
+	else:
+		name = 'Аноним'
+
+	return render(request, 'shop/index.html', {'username_name': name})
 
 
 def catalogue(request):
@@ -29,6 +36,7 @@ def get_product(request, product_id):
 def print_review_error(request, product_id):
 	return render(request, 'shop/comment_error.html', {'product_id': product_id})
 
+
 def post_review(request, product_id):
 	try:
 		product = Product.objects.get(id=product_id)
@@ -39,3 +47,30 @@ def post_review(request, product_id):
 	elif request.POST['text'] == '':
 		print_review_error(request, product_id)
 	return HttpResponseRedirect(reverse('product_by_id', kwargs={'product_id': product.id}))
+
+
+def register_page(request):
+	return render(request, 'shop/register.html')
+
+
+def log_in_page(request):
+
+	if request.method == 'POST':  # переданы данные форм
+		logout(request)  # ?
+		form = LoginForm(request.POST)
+		if form.is_valid():
+			username = form.cleaned_data['username']
+			password = form.cleaned_data['password']
+			user = authenticate(username=username, password=password)
+			if user is not None:  # если пользователь с таким логином и паролем существует
+				login(request, user)  # "прикрепляем" аутентифицированного пользователя к текущей сессии
+
+				return redirect(reverse('index'))
+
+			else:  # если пользователя такого нет
+				form.add_error(None, 'No such user!')
+
+	elif request.method == 'GET':  # обычный запрос страницы
+		form = LoginForm()
+
+	return render(request, 'shop/login.html', {'form': form})
